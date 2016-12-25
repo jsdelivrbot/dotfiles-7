df4e514a09a48e06c103a662be83a49f4789f615
Charlie Owen <charlie@sonniesedge.co.uk>
2016-12-25 22:52:22 +0100

    Remove test for oh-my-zsh, as it runs test itself

--- install.sh
+++ install.sh
@@ -3,9 +3,7 @@
 echo "Setting up your Mac..."
 
 # Install oh-my-zsh
-if test ! $(zsh --version); then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
