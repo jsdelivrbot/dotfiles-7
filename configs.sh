b423f9967bee31ff3304cb53e5c98ed99d6f4fe6
Charlie Owen <charlie@sonniesedge.co.uk>
2018-07-27 13:49:08 +0200

    Change config dir name

--- configs.sh
+++ configs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Symlink config files
-ln -s $(PWD)/configs/notes ~/.config/notes/config
-ln -s $(PWD)/configs/zshrc ~/.zshrc
-ln -s $(PWD)/configs/jrnl ~/.jrnl_config
+ln -s $(PWD)/config-files/notes ~/.config/notes/config
+ln -s $(PWD)/config-files/zshrc ~/.zshrc
+ln -s $(PWD)/config-files/jrnl ~/.jrnl_config
